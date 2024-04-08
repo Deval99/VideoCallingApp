a99be5849442cc3c550f57511f3c8c0ca225ae45
Deval <deval.k@techroversolutions.com>
2024-04-08 21:51:20 +0530

    Initial Commit

---
+++ VideoCallingApp/VideoCallingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCallingAppApp.swift
+//  VideoCallingApp
+//
+//  Created by Jaxxi.ai on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoCallingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
